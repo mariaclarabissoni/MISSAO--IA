1b69b92d3ea6e3fee803d1d85d299cbf75c3ce53
mariaclarabissoni <137806346+mariaclarabissoni@users.noreply.github.com>
2025-06-03 11:35:44 -0300

    kj

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
-const caixaPrincipal= document
-
-
-
-
-
-
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
 
 
 const perguntas = [
